10f6a34697fdc20a8c4fe3e2fbd6f126d5950478
RashidMmdov <tu7lw2f82@code.edu.az>
2022-04-03 13:45:53 +0400

    NewDelegateTask

---
+++ Delegate task/Delegate task/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Delegate_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] PruductPrices = { 1.5, 20.7, 7.99, 4.1, 100.98, 12.5 };
+            Action<double[]> FullInfo = FindTotalPriceAndCount;
+            FullInfo(PruductPrices);
+
+        }
+        public static void FindTotalPriceAndCount(double[] Arr)
+        {
+            int count = Arr.Length;
+            double TotalPrice = default(int);
+            foreach (double item in Arr)
+            {
+                TotalPrice += item;
+            }
+            Console.WriteLine($"Total price of pruduct: {TotalPrice}$ , Pruducts count: {count}");
+        }
+    }
+}
